38078956e1191392faf0ba220813a0077d7e6517
GBADAMASSI Abdel <gbadamassiabdelkabir@gmail.com>
2024-08-20 13:15:09 +0100

    Liaison backend

--- frontend/axiosConfig.js
+++ frontend/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3010/api',
+  baseURL: 'https://tokpelocation-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
